1b07350bf5cb5ab5ef5b05d8bc20183b3dad80eb
ChrisCoder9000 <alch.infoemail@gmail.com>
2022-08-26 11:55:12 +0200

    Finished the mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,79 @@
 import { StyledHeader } from "./styles/StyledHeader";
 
+import hamburgherOpen from "../assets/hamburgherOpen.png";
+import hamburgherClose from "../assets/hamburgherClose.png";
+import headerArrow from "../assets/headerArrow.png";
+
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 const Header = (props) => {
+	const [menuClicked, setMenuClicked] = useState(false);
+
+	const menuHandler = () => {
+		setMenuClicked(!menuClicked);
+	};
+
 	return (
 		<StyledHeader
 			isScrolled={props.isScrolled}
 			primaryColor={props.primaryColor}
 		>
-			<span className='headerLogo'>DLAZ</span>
-			<nav className='nav'>
-				<a className='navSpan' href='#who'>
-					Who is Daniel Lazarus ?
-				</a>
-				<a className='navSpan' href='#tiers'>
-					Monthly Coaching Subscriptions
-				</a>
-				<a className='navSpan' href='#tiers'>
-					Why You Should Enroll?
-				</a>
-			</nav>
+			<div className='headerWeb'>
+				<span className='headerLogo'>DLAZ</span>
+				<nav className='nav'>
+					<a className='navSpan' href='#who'>
+						Who is Daniel Lazarus ?
+					</a>
+					<a className='navSpan' href='#tiers'>
+						Monthly Coaching Subscriptions
+					</a>
+					<a className='navSpan' href='#tiers'>
+						Why You Should Enroll?
+					</a>
+				</nav>
+				<img
+					src={menuClicked ? hamburgherClose : hamburgherOpen}
+					alt='menu'
+					className='hamburgherMenu'
+					onClick={menuHandler}
+				/>
+			</div>
+			<div className='headerMobile'>
+				<div className={`mainHeader ${menuClicked ? "open" : "closed"}`}>
+					<span className='headerLogo'>DLAZ</span>
+					<div className='navHam'>
+						<div className='divHam'>
+							<img
+								src={menuClicked ? hamburgherClose : hamburgherOpen}
+								alt='menu'
+								className='hamburgherMenu'
+								onClick={menuHandler}
+							/>
+						</div>
+					</div>
+				</div>
+				<nav className={menuClicked ? "navOpen" : "navClosed"}>
+					<div className='linkDiv'>
+						<a className='navSpan' href='#who'>
+							Who is Daniel Lazarus ?
+						</a>
+						<img src={headerArrow} alt='arrow' className='headerArrow' />
+					</div>
+					<div className='linkDiv'>
+						<a className='navSpan' href='#tiers'>
+							Monthly Coaching Subscriptions
+						</a>
+						<img src={headerArrow} alt='arrow' className='headerArrow' />
+					</div>
+					<div className='linkDiv'>
+						<a className='navSpan' href='#tiers'>
+							Why You Should Enroll?
+						</a>
+						<img src={headerArrow} alt='arrow' className='headerArrow' />
+					</div>
+				</nav>
+			</div>
 		</StyledHeader>
 	);
 };
--- src/components/styles/StyledHeader.js
+++ src/components/styles/StyledHeader.js
@@ -1,17 +1,32 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-	width: 100%;
-	background-color: ${(props) => (props.isScrolled ? props.primaryColor : "")};
-	height: 9.35vh;
-	align-items: center;
-	display: flex;
-	font-size: 1rem;
-	color: ${({ theme }) => theme.colors.secondary};
-	justify-content: space-between;
-	position: fixed;
+	@media (min-width: 1080px) {
+		.headerMobile {
+			display: none;
+		}
+	}
 
-	z-index: 1;
+	.headerWeb {
+		width: 100%;
+		background-color: ${(props) =>
+			props.isScrolled ? props.primaryColor : ""};
+		height: 9.35vh;
+		align-items: center;
+		display: flex;
+		font-size: 1rem;
+		color: ${({ theme }) => theme.colors.secondary};
+		justify-content: space-between;
+		position: fixed;
+
+		z-index: 1;
+	}
+
+	@media (min-width: 1080px) {
+		.hamburgherMenu {
+			display: none;
+		}
+	}
 
 	.nav {
 		margin-right: 8.33vw;
@@ -31,6 +46,88 @@ export const StyledHeader = styled.header`
 		&:hover {
 			text-decoration: underline;
 			cursor: pointer;
+		}
+	}
+
+	@media (max-width: 1079px) {
+		.headerWeb {
+			display: none;
+		}
+
+		.headerMobile {
+			width: 100%;
+			position: fixed;
+
+			z-index: 1;
+		}
+
+		.mainHeader {
+			width: 100%;
+			background-color: ${(props) =>
+				props.isScrolled ? props.primaryColor : ""};
+			height: 9.35vh;
+			align-items: center;
+			display: flex;
+			font-size: 1rem;
+			color: ${({ theme }) => theme.colors.secondary};
+			justify-content: space-between;
+
+			padding-top: 3.8vh;
+			padding-bottom: 3.8vh;
+		}
+
+		.open {
+			background-color: ${({ theme }) => theme.colors.primary};
+		}
+
+		.closed {
+			background-color: transparent;
+		}
+
+		.headerLogo {
+			font-size: 35px;
+			font-weight: 500;
+			margin-left: 8.33vw;
+		}
+
+		.navHam {
+			.divHam {
+				text-align: -webkit-right;
+
+				.hamburgherMenu {
+					height: 35px;
+					margin-right: 8.33vw;
+				}
+			}
+		}
+
+		.navOpen {
+			position: relative;
+			display: grid;
+			height: 30.11vh;
+			background-color: ${({ theme }) => theme.colors.gray};
+			font-size: 1.5rem;
+			text-align: left;
+			align-content: space-around;
+			padding-left: 8.33vw;
+
+			.linkDiv {
+				display: inline-flex;
+				justify-content: space-between;
+				align-items: center;
+
+				.headerArrow {
+					height: 1.313rem;
+					text-align: right;
+					padding-right: 8.33vw;
+				}
+			}
+		}
+
+		.navClosed {
+			position: relative;
+			top: -300px;
+			display: none;
 		}
 	}
 `;
