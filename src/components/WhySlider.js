aceb644ab73bbe1c407df261f4a87e14fb453c23
ChrisCoder9000 <alch.infoemail@gmail.com>
2022-08-27 11:48:42 +0200

    Finished the creation of the WhySlider. NEXT: -maybe add some animation for the slider -add links to the arrows on the heaer -review both the mobile version and the mobile for any aventual modification

--- src/components/WhySlider.js
+++ src/components/WhySlider.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
 import { StyledWhySlider } from "./styles/StyledWhySlider";
+import TitleComponent from "./TitleComponent";
+
+const data = [
+	{ number: 4, text: "Years of Experience" },
+	{ number: 4, text: "Millions Dollars Earned" },
+	{ number: 3, text: "Bracelets Won" },
+];
 
 const WhySlider = () => {
-	return <StyledWhySlider></StyledWhySlider>;
+	const [index, setIndex] = useState(1);
+
+	const leftArrowHandler = () => {
+		if (index > 0) {
+			setIndex((prevState) => prevState - 1);
+		} else {
+			setIndex(2);
+		}
+		console.log(index);
+	};
+
+	const rightArrowHandler = () => {
+		if (index < data.length - 1) {
+			setIndex((prevState) => prevState + 1);
+		} else {
+			setIndex(0);
+		}
+		console.log(index);
+	};
+
+	return (
+		<StyledWhySlider>
+			<TitleComponent text='WHY' />
+			<div className='sliderDiv'>
+				<span className='arrows' onClick={leftArrowHandler}>
+					&lsaquo;
+				</span>
+				<div className='keySlide'>
+					<h1 className='number'>{data[index].number}</h1>
+					<span className='span'>{data[index].text}</span>
+				</div>
+				<span className='arrows' onClick={rightArrowHandler}>
+					&rsaquo;
+				</span>
+			</div>
+		</StyledWhySlider>
+	);
 };
 
 export default WhySlider;
--- src/components/styles/StyledWhySlider.js
+++ src/components/styles/StyledWhySlider.js
@@ -1,3 +1,34 @@
 import styled from "styled-components";
 
-export const StyledWhySlider = styled.div``;
+export const StyledWhySlider = styled.div`
+	margin-block: 18vh;
+
+	.sliderDiv {
+		display: flex;
+		align-items: baseline;
+		width: auto;
+		justify-content: space-between;
+
+		.arrows {
+			font-size: 15vh;
+			margin-inline: 5vw;
+
+			&:hover {
+				color: ${({ theme }) => theme.colors.accent};
+			}
+		}
+
+		.keySlide {
+			.number {
+				font-family: "din-condensed", sans-serif;
+				font-size: 10.44rem;
+				font-weight: 400;
+				margin-bottom: 0;
+			}
+
+			.span {
+				font-size: 1.938rem;
+			}
+		}
+	}
+`;
